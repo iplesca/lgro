d24ae013264d23531845551eb9c4919869739b45
Ionut Plesca <sirlucas_ro@yahoo.com>
2017-12-01 08:05:38 +0000

    Initial

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.main')
+
+@section('content')
+    Un Clan bazat pe Respect, Prietenie si Altruism.<br>
+    Comunicare prin TeamSpeak3 pe adresa: <strong>piroman.isteam.ro</strong>
+    <h3>Condiții de recrutare</h3>
+    <ul>
+        <li>experiență de luptă în echipă</li>
+        <li>WN8 recent (30 de zile): preferabil +1200</li>
+        <li>minim un tanc de <strong>Tier 10</strong> din cele de mai jos:<br>
+            <ul class="recruit-tank-list">
+                <li>
+                    Tancuri grele [HT]<br>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:R45_IS-7">
+                        <span>IS-7</span>
+                        <img src="http://wiki.gcdn.co/images/6/64/AnnoR45_IS-7.png"></a>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:A69_T110E5">
+                        <span>T110<strong>E5</strong></span>
+                        <img src="http://wiki.gcdn.co/images/b/bc/AnnoA69_T110E5.png"></a>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:Ch22_113">
+                        <span>113</span>
+                        <img src="http://wiki.gcdn.co/images/7/79/AnnoCh22_113.png"></a>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:Ch41_WZ_111_5A">
+                        <span>WZ-111 model 5A</span>
+                        <img src="http://wiki.gcdn.co/images/6/6b/AnnoCh41_WZ_111_5A.png"></a>
+                    <span class="clearfix"></span>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:S16_Kranvagn">
+                        <span>Kranvagn</span>
+                        <img src="http://wiki.gcdn.co/images/2/26/AnnoS16_Kranvagn.png"></a>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:F10_AMX_50B">
+                        <span>AMX 50 B</span>
+                        <img src="http://wiki.gcdn.co/images/e/e6/AnnoF10_AMX_50B.png"></a>
+                    <span class="clearfix"></span>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:G42_Maus">
+                        <span>Maus</span>
+                        <img src="http://wiki.gcdn.co/images/2/2a/AnnoG42_Maus.png"></a>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:J20_Type_2605">
+                        <span>Type 5 Heavy</span>
+                        <img src="http://wiki.gcdn.co/images/2/22/AnnoJ20_Type_2605.png"></a>
+                </li>
+                <li>
+                    Tancuri medii [MT]<br>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:R97_Object_140">
+                        <span>Obj. 140</span>
+                        <img src="http://wiki.gcdn.co/images/e/e6/AnnoR97_Object_140.png"></a>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:R96_Object_430">
+                        <span>Obj. 430</span>
+                        <img src="http://wiki.gcdn.co/images/5/55/AnnoR96_Object_430.png"></a>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:R95_Object_907">
+                        <span>Obj. 907</span>
+                        <img src="http://wiki.gcdn.co/images/0/08/AnnoR95_Object_907.png"></a>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:Ch19_121">
+                        <span>121</span>
+                        <img src="http://wiki.gcdn.co/images/0/02/AnnoCh19_121.png"></a>
+                    <span class="clearfix"></span>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:F18_Bat_Chatillon25t">
+                        <span>BC 25t</span>
+                        <img src="http://wiki.gcdn.co/images/6/61/AnnoF18_Bat_Chatillon25t.png"></a>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:Cz04_T50_51">
+                        <span>TVP T 50/51</span>
+                        <img src="http://wiki.gcdn.co/images/d/d4/AnnoCz04_T50_51.png"></a>
+                </li>
+                <li>
+                    Anti-tanc [TD]<br>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:A85_T110E3">
+                        <span>T110<strong>E3</strong></span>
+                        <img src="http://wiki.gcdn.co/images/1/16/AnnoA85_T110E3.png"></a>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:GB48_FV215b_183">
+                        <span>FV<strong>215b</strong></span>
+                        <img src="http://wiki.gcdn.co/images/6/66/AnnoGB48_FV215b_183.png"></a>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:S11_Strv_103B">
+                        <span>Strv<strong>103B</strong></span>
+                        <img src="http://wiki.gcdn.co/images/2/2c/AnnoS11_Strv_103B.png"></a>
+                </li>
+                <li>
+                    Artilerie<br>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:GB31_Conqueror_Gun">
+                        <span>Conqueror GC</span>
+                        <img src="http://wiki.gcdn.co/images/f/f9/AnnoGB31_Conqueror_Gun.png"></a>
+                    <a target="_blank" class="med-tank" href="http://wiki.wargaming.net/en/Tank:A38_T92">
+                        <span>T92 HMC</span>
+                        <img src="http://wiki.gcdn.co/images/4/49/AnnoA38_T92.png"></a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+@endsection
