63cff1389a69435058006a29889e7e3720fbf990
Ionut Plesca <sirlucas_ro@yahoo.com>
2017-12-24 10:54:26 +0000

    Moving WG settings to env

--- config/wotapi.php
+++ config/wotapi.php
@@ -1,9 +1,6 @@
 <?php
 return [
-    'base_uri'               => 'https://api.worldoftanks.eu',
-    'application_id'         => '2ff99442e997bc8794ae14379396211e',
-    'default_realm'          => 'eu',
-    'redirect_uri'           => env('APP_DEBUG') ?
-        'http://3ddbf604.ngrok.io/wot_redirect/'
-        : 'http://lg-ro.isteam.ro/auth_wargaming/'
+    'application_id'         => env('WG_APPLICATION_ID'),
+    'default_realm'          => env('WG_REALM', 'eu'),
+    'redirect_uri'           => env('WG_REDIRECT_URI') . '/wot_redirect/'
 ];
