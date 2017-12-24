f1a4baceefba1d4cb84a8f85e0c4267d852e4560
Ionut Plesca <sirlucas_ro@yahoo.com>
2017-12-24 12:44:28 +0000

    everyHour -> hourly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new CheckClanMembers)->everyHour();
+        $schedule->job(new CheckClanMembers)->hourly();
     }
 
     /**
