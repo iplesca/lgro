3ed59250fead21fea261ad1de7a64d530fee94ef
Ionut Plesca <sirlucas_ro@yahoo.com>
2017-12-24 10:56:34 +0000

    Fix WG redirect URL

--- config/wotapi.php
+++ config/wotapi.php
@@ -2,5 +2,5 @@
 return [
     'application_id'         => env('WG_APPLICATION_ID'),
     'default_realm'          => env('WG_REALM', 'eu'),
-    'redirect_uri'           => env('WG_REDIRECT_URI') . '/wot_redirect/'
+    'redirect_uri'           => env('WG_REDIRECT_URI')
 ];
